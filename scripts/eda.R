8f2ae4e98e531204e006b07fa814aa30adb871fa
Conor <conorotompkins@gmail.com>
2019-07-07 10:26:45 -0400

    updated eda code with chart to look at which sites collect which measurements

--- scripts/eda.R
+++ scripts/eda.R
@@ -1,3 +1,5 @@
+#devtools::install_github("tidyverse/tidyr")
+
 library(tidyverse)
 library(lubridate)
 
@@ -13,6 +15,42 @@ df %>%
 
 df %>% 
   count(stat)
+
+df %>% 
+  count(stat, no2, sort = TRUE)
+
+#not working
+df %>% 
+  select(starts_with("pm")) %>% 
+  pivot_longer(names_to = "measure", values_to = "value")
+  count()
+
+df %>% 
+  select(site, datetime, stat,
+         bcstat, h2s, 
+         no, no2, nox, noy, noydif,
+         out_rh, out_t, ozone, ozone2,
+         contains("pm"),
+         #pm10, pm10b, pm25, pm25b, pm25t, 
+         so2#,
+         #uvpm, rainfall, solarrad,
+         #rwd, rws, sigtheta, sonicwd, sonicws, int_t
+         ) %>% 
+  select(site:stat, bcstat:so2) %>% 
+  tidyr::pivot_longer(-c(site, datetime, stat), names_to = "measure", values_to = "value") %>% 
+  filter(!is.na(value)) %>% 
+  count(site, stat, measure, sort = TRUE) %>% 
+  mutate(stat = as.factor(stat),
+         measure = as.factor(measure)) %>% 
+  #filter(measure %in% c("pm25", "pm10")) %>% 
+  #filter(measure == "no2") %>% 
+  ggplot(aes(stat, site, fill = n)) +
+  geom_tile(color = "black") +
+  scale_fill_viridis_c() +
+  scale_x_discrete(expand = c(0,0)) +
+  scale_y_discrete(expand = c(0,0)) +
+  facet_wrap(~measure, nrow = 1) +
+  theme(panel.grid = element_blank())
 
 df %>% 
   ggplot(aes(h2s)) +
